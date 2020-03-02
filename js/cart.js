269a6e39a7cdaf71abd18f11c4e8bca24fe8158c
Razu Ahmed <razu.exe@gmail.com>
2020-03-02 21:22:11 +0600

    design and implement done

--- js/cart.js
+++ js/cart.js
@@ -23,7 +23,7 @@ incrementOne.addEventListener("click", function () {
 // Increment second row
 
 const incrementTwo = document.getElementById("increment2");
-incrementTwo.addEventListener("click", function(){
+incrementTwo.addEventListener("click", function () {
     const boxetwoValue = document.getElementById("boxValue2").value++;
     const boxetwoValueInt = parseFloat(boxetwoValue);
     const iphoneCase = document.getElementById("iphoneCase").innerText;
@@ -46,10 +46,90 @@ incrementTwo.addEventListener("click", function(){
 // Decrement first row
 const decrementOne = document.getElementById("decrement1");
 decrementOne.addEventListener("click", function () {
-    const decboxeoneValue = document.getElementById("boxValue1").value;
+    const decboxeoneValue = document.getElementById("boxValue1").value--;
+    if (decboxeoneValue == 0) {
+        document.getElementById("boxValue1").value++;
+        iphoneValueDec = 799;
+    }
     const decboxeoneValueInt = parseFloat(decboxeoneValue);
+    const iphoneDec = document.getElementById("iphonePrice").innerText;
+    const iphoneValueDec = parseFloat(iphoneDec);
+    const phonePriceupdateDec = iphoneValueDec - 799;
+    document.getElementById("iphonePrice").innerText = phonePriceupdateDec;
 
-    console.log("bal ki")
-    
+    const subTotalDec = document.getElementById("subTotal").innerText;
+    const subtotalValueDec = parseFloat(subTotalDec);
+    const subtotalUpdateDec = subtotalValueDec - 799;
+    document.getElementById("subTotal").innerText = subtotalUpdateDec;
+
+    const totalpriceDec = document.getElementById("totalPrice").innerText;
+    const totalValueDec = parseFloat(totalpriceDec);
+    const talUpdateDec = totalValueDec - 799;
+    document.getElementById("totalPrice").innerText = talUpdateDec;
+
+
+})
+
+// Decrement second row
+const decrementTwo = document.getElementById("decrement2");
+decrementTwo.addEventListener("click", function () {
+    const decboxeoneValuee = document.getElementById("boxValue2").value--;
+    if (decboxeoneValuee == 0) {
+        document.getElementById("boxValue2").value++;
+        iphoneValueDece = 131;
+    }
+    const decboxeoneValueInte = parseFloat(decboxeoneValuee);
+    const iphoneCasee = document.getElementById("iphoneCase").innerText;
+    const iphoneValueDece = parseFloat(iphoneCasee);
+    const phonePriceupdateDece = iphoneValueDece - 131;
+    document.getElementById("iphoneCase").innerText = phonePriceupdateDece;
+
+    const subTotalDece = document.getElementById("subTotal").innerText;
+    const subtotalValueDece = parseFloat(subTotalDece);
+    const subtotalUpdateDece = subtotalValueDece - 131;
+    document.getElementById("subTotal").innerText = subtotalUpdateDece;
+
+    const totalpriceDece = document.getElementById("totalPrice").innerText;
+    const totalValueDece = parseFloat(totalpriceDece);
+    const talUpdateDece = totalValueDece - 131;
+    document.getElementById("totalPrice").innerText = talUpdateDece;
+
+})
+// Remove first row
+const removeRow = document.getElementById("removeBtn1");
+removeRow.addEventListener("click", function () {
+    const removeValue = document.getElementById("iphonePrice").innerText;
+    const removeValueNumber = parseFloat(removeValue);
+
+    const subTotalRemove = document.getElementById("subTotal").innerText;
+    const subtotalValueRemove = parseFloat(subTotalRemove);
+    const subtotalUpdateRemove = subtotalValueRemove - removeValueNumber;
+    document.getElementById("subTotal").innerText = subtotalUpdateRemove;
+
+    const totalRemove = document.getElementById("totalPrice").innerText;
+    const totalValueRemove = parseFloat(totalRemove);
+    const totalUpdateRemove = totalValueRemove - removeValueNumber;
+    document.getElementById("totalPrice").innerText = totalUpdateRemove;
+    document.getElementById("row1").style.display = 'none'
+
+})
+
+
+// Remove second row
+const removeRoww = document.getElementById("removeBtn2");
+removeRoww.addEventListener("click", function () {
+    const removeValuew = document.getElementById("iphoneCase").innerText;
+    const removeValueNumberw = parseFloat(removeValuew);
+
+    const subTotalRemovew = document.getElementById("subTotal").innerText;
+    const subtotalValueRemovew = parseFloat(subTotalRemovew);
+    const subtotalUpdateRemovew = subtotalValueRemovew - removeValueNumberw;
+    document.getElementById("subTotal").innerText = subtotalUpdateRemovew;
+
+    const totalRemovew = document.getElementById("totalPrice").innerText;
+    const totalValueRemovew = parseFloat(totalRemovew);
+    const totalUpdateRemovew = totalValueRemovew - removeValueNumberw;
+    document.getElementById("totalPrice").innerText = totalUpdateRemovew;
+    document.getElementById("row2").style.display = 'none'
 
 })
